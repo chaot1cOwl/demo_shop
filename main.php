1f4b328f07d785b66c4c60a19c96a2aa77e87c5e
chaoticowl <owl.of.lime@gmail.com>
2026-02-17 10:40:25 +0500

    fix carousel height

--- main.php
+++ main.php
@@ -1,18 +1,21 @@
 <!-- Пример карусели на базе bootstrap. Подробнее: https://getbootstrap.com/docs/5.3/components/carousel/ -->
-<div id="carouselExampleRide" class="carousel slide carousel-shadow" data-bs-ride="true">
+<div id="mainPageBanners" class="carousel carousel-dark slide carousel-shadow main-carousel" data-bs-ride="false" >
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="files/images/banner1.png" class="d-block w-100" alt="Баннер 1">
+      <img src="files/images/banner1.png" class="d-block" alt="Баннер 1">
     </div>
     <div class="carousel-item">
-      <img src="files/images/banner2.png" class="d-block w-100" alt="Баннер 2">
+      <img src="files/images/banner2.png" class="d-block" alt="Баннер 2">
+    </div>
+    <div class="carousel-item">
+      <img src="files/images/donut1.png" class="d-block" alt="Баннер 3">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#mainPageBanners" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#mainPageBanners" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
